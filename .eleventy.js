b44d4d4c438d80b7a4b63d23e99cdd793ad2f5f8
dandevri <hi@dandevri.es>
2020-08-27 15:02:20 +0200

    Rename _site > docs

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('style')
   eleventyConfig.addPassthroughCopy("img");
-  
+
   return {
     passthroughFileCopy: true,
     dir: {
       includes: "_includes",
       data: "_data",
-      output: "_site"
+      output: "docs"
     }
   }
 }
