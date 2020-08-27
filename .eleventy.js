4bc9ccafb389d2ca6523484ce9720e074d0dd75c
Robert Spier <robert.spier@gmail.com>
2020-08-27 11:42:24 +0200

    Change pages directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('style')
 	eleventyConfig.addPassthroughCopy("img");
 	return {
+		dir: {
+			input: "pages"
+		},
 		passthroughFileCopy: true
 	}
 }
